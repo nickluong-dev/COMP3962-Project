b4d9a0b9139b45a76b201eaaca292639c1396889
nickluong97@outlook.com <nickluong97@outlook.com>
2022-03-26 03:54:43 -0700

    Added navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,17 @@
 <template>
   <div
-    id="nav"
-    class="outline outline-black outline-1 flex items-end bg-violet-700"
-  >
-    <h1 class="title">PokeMart</h1>
-    <router-link to="/" class="nav-item outline-1">Home</router-link>
-    |
-    <router-link to="/about" class="nav-item">About</router-link>
+    id="navBar"
+    class="nav bg-rose-400 p-3">
+    
+    <section class="flex justify-between items-end">
+      <h1 class="title text-rose-100"><span class="pokemons text-amber-300 text-4xl pl-3 pr-10">R</span>PokeMart</h1> 
+      <section class="flex">
+        <router-link to="/" class="nav-item text-rose-100">Store</router-link> <p class="nav-item text-rose-100">|</p>
+        <router-link to="/" class="nav-item text-rose-100">Cart</router-link> 
+      </section>
+    </section>
+    <!-- <div class="pokemons text-black tracking-widest text-2xl py-3">gdfxahty412hsdfampqwfbxcz0941</div> -->
+    
   </div>
   <router-view />
 </template>
@@ -19,15 +24,24 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
+<style scoped>
 .title {
   font-family: "Pokemon Classic";
   font-size: 3em;
 }
 
-.nav-item {
+.nav-item, .nav-item:visited {
   font-family: "Pokemon Classic";
-  margin: 0 1em 0 1em;
-  font-size: 1.2em;
+  margin: 0 0.5em 0 0.5em;
+  font-size: 1.5em;
+  margin-bottom: 0.2em;
+}
+
+.router-link-exact-active:active{
+    color: rgb(252 211 77);
+}
+
+.pokemons{
+  font-family: "Pokemon Pixels 1";
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
